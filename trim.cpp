20be1b612f8a57d3d2d083aaa35d720e20a3e528
ivonnegattringer <ivonne.gattringer@gmail.com>
2017-12-07 15:52:13 +0100

    trim.cpp Done

--- trim.cpp
+++ trim.cpp
@@ -15,17 +15,30 @@
 #include <stdbool.h>
 
 void  trim(const char *source, char *trimmed_string){
+  trimmed_string = fill_trimmed_string_right(source, trimmed_string);
+}
+int blanks_at_the_beginning(const char *source){
   int readPosition = 0;
   while(source[readPosition]== ' ') {
     readPosition++;
   }
-
+  return readPosition;
+}
+int where_to_end(const char *source){
   int max_len = strlen(source);
-  int insertPosition = 0;
-  while(readPosition < max_len && !(source[readPosition] == ' ' && (source[readPosition +1] == ' ' || source[readPosition + 1] == '\0'))){
-    trimmed_string[insertPosition] = source[readPosition];
-    readPosition++;
-    insertPosition++;
+  while(source[max_len-1] == ' '){
+    max_len--;
   }
-  trimmed_string[insertPosition] = '\0';
+  return max_len;
+}
+char* fill_trimmed_string_right(const char *source, char *trimmed_string){
+    int insertPosition = 0;
+    int max_len = where_to_end(source);
+    int readPosition = blanks_at_the_beginning(source);
+      for (; readPosition < max_len; readPosition++) {
+          trimmed_string[insertPosition] = source[readPosition];
+          insertPosition++;
+      }
+      trimmed_string[insertPosition] = '\0';
+      return trimmed_string;
 }
